48e606ebdb79e0410f0e8b8b08849b7df6fc4781
swatihere <swatisriivastava9@gmail.com>
2026-08-13 00:09:19 +0530

    Heaps

--- src/Heap/KthSmallest.java
+++ src/Heap/KthSmallest.java
@@ -1,5 +1,19 @@
 package Heap;
 
-public class KthSmallest {  //here we use Max HEap
+import java.util.Collections;
+import java.util.PriorityQueue;
 
+public class KthSmallest {  //here we use Max Heap
+    public static int kthSmallest(int [] arr , int k ) {
+        //Max Heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int ele : arr){  //nlogk
+            pq.add(ele);
+
+            if(pq.size()>k){
+                pq.remove(); //logk
+            }
+        }
+        return pq.peek();
+    }
 }
